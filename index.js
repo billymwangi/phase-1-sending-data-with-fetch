ce1d06098ccd63de7cd5eaad58c21715622d9dc2
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-19 08:52:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const submitData = (name,email)=>{
+  const users = {name:name, email:email}
+
+  return fetch ("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept:"application/json",
+    },
+    body: JSON.stringify(users)
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.body.innerHTML=data.id
+  })
+  .catch(error => {
+    document.body.innerHTML=error.message
+    alert("name is incorrect")
+  })
+
+
+}
